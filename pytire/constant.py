93287765fdee4540ae9ebeb5d9af8886361b4790
opensomersault <abrobinson1907@gmail.com>
2021-02-24 21:22:50 +0000

    add constants

---
+++ pytire/constant.py
@@ -0,0 +1,11 @@
+"""
+Defines constants used elsewhere in the library.
+
+:author: Alex Robinson <girotobial@gmail.com>
+:copyright: Copyright (c) Alex Robinson, 2021-2021.
+:license: MIT
+"""
+
+
+INCHES_PER_FOOT = 12
+FEET_PER_METER = 3.28084
