e0060bef0ce679c6dd1812bd29aaadb1beddf696
opensomersault <abrobinson1907@gmail.com>
2021-03-01 18:08:25 +0000

    tests: add tordiol volumes

--- tests/test_tire.py
+++ tests/test_tire.py
@@ -72,8 +72,56 @@ def test_should_detect_correct_unit(size, expected_unit):
         ("H44.5x16.5-21", 0.474),
     ],
 )
-def test_should_calculate_cuboid_filled_volume(size, expected_value):
+def test_should_calculate_cuboid_volume(size, expected_value):
     tire = Tire(size)
-    assert tire.volume(geometry="cuboid", center_hole_filled=True) == pytest.approx(
+    assert tire.volume(geometry="cuboid") == pytest.approx(expected_value)
+
+
+@pytest.mark.parametrize(
+    ("size", "expected_value"),
+    [
+        ("H30x9.50-16", 0.110),
+        ("27x7.75-15", 0.073),
+        ("615x225-10", 0.067),
+        ("12.50-16", None),
+        ("18X5.5", 0.023),
+        ("H44.5x16.5-21", 0.421),
+    ],
+)
+def test_should_calculate_cylinder_volume(size, expected_value):
+    tire = Tire(size)
+    assert tire.volume(geometry="cylinder", center_hole_filled=True) == pytest.approx(
         expected_value
     )
+
+
+@pytest.mark.parametrize(
+    ("size", "expected_value"),
+    [
+        ("H30x9.50-16", 0.110),
+        ("27x7.75-15", 0.073),
+        ("615x225-10", 0.067),
+        ("12.50-16", None),
+        ("18X5.5", 0.023),
+        ("H44.5x16.5-21", 0.421),
+    ],
+)
+def test_should_calculate_circular_toroid_volume(size, expected_value):
+    tire = Tire(size)
+    assert tire.volume(geometry="circular_toroid") == pytest.approx(expected_value)
+
+
+@pytest.mark.parametrize(
+    ("size", "expected_value"),
+    [
+        ("H30x9.50-16", 0.0456),
+        ("27x7.75-15", 0.0306),
+        ("615x225-10", 0.0349),
+        ("12.50-16", None),
+        ("18X5.5", None),
+        ("H44.5x16.5-21", 0.1828),
+    ],
+)
+def test_should_calculate_square_toroid_volume(size, expected_value):
+    tire = Tire(size)
+    assert tire.volume(geometry="square_toroid") == pytest.approx(expected_value)
