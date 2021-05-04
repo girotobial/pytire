dc39847243880ccd006ec1b574e6473870870bf7
Alex Robinson <girotobial@gmail.com>
2021-05-04 16:59:37 +0000

    :recycle: abstract size properties to size class

---
+++ pytire/size.py
@@ -0,0 +1,115 @@
+import abc
+import re
+from typing import Optional
+
+from pytire.constant import DIAMETER_RE, METRIC_RE, WHEEL_DIAMETER_RE, WIDTH_RE
+from pytire.enums import Unit
+from pytire.geometry import ThreeDimensionalShape, convert_length, create_shape
+
+
+class Size(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def __init__(self, size: str):
+        raise NotImplementedError
+
+    @property
+    @abc.abstractmethod
+    def outer_diameter(self) -> Optional[float]:
+        raise NotImplementedError
+
+    @property
+    @abc.abstractmethod
+    def width(self) -> Optional[float]:
+        raise NotImplementedError
+
+    @property
+    @abc.abstractmethod
+    def rim_diameter(self) -> Optional[float]:
+        raise NotImplementedError
+
+    def volume(self, geometry: str = "cuboid") -> Optional[float]:
+        """The exterior volume of the tire.
+
+        Parameters
+        ----------
+        geometry : str, default 'cuboid'
+            The shape assumed during the calculation of the volume.
+            allowed values are ['cuboid', 'cylinder', 'square_toroid', 'torus']
+
+        Returns
+        -------
+        float
+            Volume in m^2
+        """
+        shape: ThreeDimensionalShape = create_shape(
+            geometry, self.outer_diameter, self.width, self.rim_diameter
+        )
+        return shape.volume()
+
+    @property
+    def aspect_ratio(self) -> Optional[float]:
+        """The ratio between the height of the tyre's sidewall to its width.
+
+        Returns
+        -------
+        Optional[float]
+            The aspect ratio to 3 decimal places.
+        """
+        if (
+            self.outer_diameter is None
+            or self.width is None
+            or self.rim_diameter is None
+        ):
+            return None
+
+        numerator = 0.5 * (self.outer_diameter - self.rim_diameter)
+
+        return round(numerator / self.width, 3)
+
+    @abc.abstractmethod
+    def __str__(self) -> str:
+        raise NotImplementedError
+
+
+class AircraftSize(Size):
+    def __init__(self, size: str):
+        self._size = size
+        self.unit = Unit.MILLIMETRE if re.match(METRIC_RE, self._size) else Unit.INCH
+
+    def __str__(self) -> str:
+        return self._size
+
+    @property
+    def outer_diameter(self) -> Optional[float]:
+        match = re.search(DIAMETER_RE, self._size)
+        if match is None:
+            return match
+
+        outer_diameter = float(match.group(0))
+        return convert_length(outer_diameter, self.unit, Unit.METRE)
+
+    @property
+    def width(self) -> Optional[float]:
+        """Size width in metres"""
+
+        match = re.search(WIDTH_RE, self._size)
+        if match is None:
+            return match
+
+        width = float(match.group(0))
+        return convert_length(width, self.unit, Unit.METRE)
+
+    @property
+    def rim_diameter(self) -> Optional[float]:
+        """Rim diameter in metres"""
+
+        match = re.search(WHEEL_DIAMETER_RE, self._size)
+        if match is None:
+            return match
+
+        wheel_diameter = float(match.group(0))
+        return convert_length(wheel_diameter, Unit.INCH, Unit.METRE)
+
+
+def size_factory(size_string: str) -> Size:
+    return AircraftSize(size_string)
--- pytire/tire.py
+++ pytire/tire.py
@@ -4,12 +4,9 @@
 :license: MIT
 """
 
-import re
 from typing import Optional
 
-from pytire.constant import DIAMETER_RE, METRIC_RE, WHEEL_DIAMETER_RE, WIDTH_RE
-from pytire.enums import Unit
-from pytire.geometry import ThreeDimensionalShape, convert_length, create_shape
+from pytire.size import size_factory
 
 
 class Tire:
@@ -31,42 +28,26 @@ class Tire:
             Size code as would be displayed on the sidewall.
             E.g 'H45.5x16.5-21', '30x10.75-16', '615x225-10'
         """
-        self.size = size
-        self.unit = Unit.MILLIMETRE if re.match(METRIC_RE, self.size) else Unit.INCH
+        self._size = size_factory(size)
+
+    @property
+    def size(self) -> str:
+        return self._size.__str__()
 
     @property
     def diameter(self) -> Optional[float]:
         """Tire diameter in metres"""
-
-        match = re.search(DIAMETER_RE, self.size)
-
-        if match is None:
-            return match
-
-        diameter = float(match.group(0))
-        return convert_length(diameter, self.unit, Unit.METRE)
+        return self._size.outer_diameter
 
     @property
     def width(self) -> Optional[float]:
         """Tire width in metres"""
-
-        match = re.search(WIDTH_RE, self.size)
-        if match is None:
-            return match
-
-        width = float(match.group(0))
-        return convert_length(width, self.unit, Unit.METRE)
+        return self._size.width
 
     @property
     def wheel_diameter(self) -> Optional[float]:
         """Wheel diameter in metres"""
-
-        match = re.search(WHEEL_DIAMETER_RE, self.size)
-        if match is None:
-            return match
-
-        wheel_diameter = float(match.group(0))
-        return convert_length(wheel_diameter, Unit.INCH, Unit.METRE)
+        return self._size.rim_diameter
 
     def volume(self, geometry: str = "cuboid") -> Optional[float]:
         """The exterior volume of the tire.
@@ -83,10 +64,7 @@ class Tire:
             Volume in m^2
         """
 
-        shape: ThreeDimensionalShape = create_shape(
-            geometry, self.diameter, self.width, self.wheel_diameter
-        )
-        return shape.volume()
+        return self._size.volume(geometry=geometry)
 
     @property
     def aspect_ratio(self) -> Optional[float]:
@@ -97,9 +75,4 @@ class Tire:
         Optional[float]
             The aspect ratio to 3 decimal places.
         """
-        if self.wheel_diameter is None or self.width is None or self.diameter is None:
-            return None
-
-        numerator = 0.5 * (self.diameter - self.wheel_diameter)
-
-        return round(numerator / self.width, 3)
+        return self._size.aspect_ratio
